651c2aacc96c0d26b1bddea685c127f9f64acf4d
Umberto Albano <u.albano@outlook.de>
2023-07-24 17:51:52 +0200

    Rename package name and change the version

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import find_packages, setup
 
-setup(name="Lib Interfaces",
-      version="1.0",
-      description="Internal lib for the important inferfaces",
+setup(name="Pfadi Apis",
+      version="0.1",
+      description="Internal lib to interact with system in a scouts context",
+      author='Umberto Albano',
+      author_email='umberto.albano@nds.pfadfinden.de',
       packages=find_packages(exclude=("test",)),
       install_requires=["python-docx" ,"html5lib","O365","openpyxl", "requests", "python-decouple","pandas","atlassian-python-api","httplib2","lxml","urllib3"],
       extras_require={
