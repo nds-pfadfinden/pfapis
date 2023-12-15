e1cd3e44620276ff02b907b3720f781e2d0fa42d
Umberto Albano <u.albano@outlook.de>
2023-12-15 14:36:35 +0100

    Add utils and a function to split addresses into street and numbert

---
+++ pfapis/utils.py
@@ -0,0 +1,23 @@
+
+def split_address(address):
+    """ Konvert a string of full address into two parts.
+    Thh String is splitted by first position a number is founded.
+    Example:
+
+    address = "Donnerschweer Str. 26a"
+
+    street, number = split_address(address) 
+    #street: Donnerschweer Str.
+    #number: 26a
+
+    """
+ 
+    split_index = None
+    for index,letter in enumerate(address):
+        if letter.isnumeric():
+            split_index = index - 1
+ 
+    street=address[0:split_index].strip()
+    number=address[split_index:].strip()
+ 
+    return street, address
